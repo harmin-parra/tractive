02febccf80870393dd771fe5953aeeaeff1c29a0
harmin-parra <harmin.parra@gmail.com>
2024-11-07 15:56:29 +0100

    improve random emails

--- data.ts
+++ data.ts
@@ -4,11 +4,15 @@ export const password = "password";
 export const firstname = "Harmin";
 export const lastname = "Parra";
 
-export function getRandomEmail(length: number = 12): string {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result + "@free.fr";
+// Get random email based on current date.
+export function getRandomEmail(): string {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}-${hours}-${minutes}-${seconds}@free.fr`;
 }
