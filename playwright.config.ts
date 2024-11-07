dfe833a008e5471f71753d5a12b244b6da2a7b5d
harmin-parra <harmin.parra@gmail.com>
2024-11-07 22:01:26 +0100

    settings for HTML reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['html']
+    ['html', { open: 'never' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
